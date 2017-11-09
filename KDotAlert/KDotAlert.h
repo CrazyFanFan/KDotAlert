e21f5a86c3c177e21c4e83c4e2eaebc65b8fc7b3
Crazy凡 <kongkk@fxiaoke.com>
2017-11-09 15:33:00 +0800

    compeleted

--- KDotAlert/KDotAlert.h
+++ KDotAlert/KDotAlert.h
@@ -6,8 +6,46 @@
 //  Copyright © 2017 Crazy凡. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface KDotAlert : NSObject
+@class KDotAlert;
+
+typedef KDotAlert * _Nonnull(^KDotAlertStringBlock)(NSString * _Nullable value);
+typedef KDotAlert * _Nonnull(^KDotAlertActionBlock)(NSString * _Nonnull title, void (^ _Nullable hanlder)(UIAlertAction * _Nonnull action));
+typedef KDotAlert * _Nonnull(^KDotAlertMakePreferredActionBlock)(void);
+typedef KDotAlert * _Nonnull(^KDotAlertTextFieldBlock)(void (^ _Nullable hanlder)(UITextField * _Nonnull textField));
+typedef KDotAlert * _Nonnull(^KDotAlertSubviewsBlock)(void (^ _Nullable hanlder)(NSArray<id> * _Nullable subViews));
+typedef KDotAlert * _Nonnull(^KDotAlertShowBlock)(UIViewController * _Nonnull viewController, void (^ _Nullable completed)(void));
+
+NS_CLASS_AVAILABLE_IOS(8_0) @interface KDotAlert : NSObject
+@property (nonatomic, strong) KDotAlertStringBlock _Nullable title;             // set title
+@property (nonatomic, strong) KDotAlertStringBlock _Nullable message;           // set message
+
+@property (nonatomic, strong) KDotAlertActionBlock _Nullable defaultAction;     // add default Action
+@property (nonatomic, strong) KDotAlertActionBlock _Nullable cancelAction;      // add cancel Action
+@property (nonatomic, strong) KDotAlertActionBlock _Nullable destructiveAction; // add destructive Action
+@property (nonatomic, strong) KDotAlertSubviewsBlock _Nullable actions;         // get all action
+@property (nonatomic, strong) KDotAlertMakePreferredActionBlock _Nullable makePreferredAction NS_AVAILABLE_IOS(9_0);     // use follow addaction to  make preferredAction
+
+@property (nonatomic, strong) KDotAlertTextFieldBlock _Nullable addTextField;   // add TextField can only use in alert;
+@property (nonatomic, strong) KDotAlertSubviewsBlock _Nullable textFields;      // get all action
+
+@property (nonatomic, strong) KDotAlertShowBlock _Nullable show;                // show with viewController
+
+
++ (instancetype _Nonnull )alert;
++ (instancetype _Nonnull )actionSheet;
+
+
+- (KDotAlertStringBlock _Nullable )title;
+- (KDotAlertStringBlock _Nullable )message;
+- (KDotAlertActionBlock _Nullable )defaultAction;
+- (KDotAlertActionBlock _Nullable )cancelAction;
+- (KDotAlertActionBlock _Nullable )destructiveAction;
+- (KDotAlertSubviewsBlock _Nullable )actions;
+- (KDotAlertMakePreferredActionBlock _Nonnull )makePreferredAction NS_AVAILABLE_IOS(9_0);
+- (KDotAlertTextFieldBlock _Nullable )addTextField;
+- (KDotAlertSubviewsBlock _Nullable )textFields;
+- (KDotAlertShowBlock _Nullable )show;
 
 @end
--- KDotAlert/KDotAlert.m
+++ KDotAlert/KDotAlert.m
@@ -8,6 +8,114 @@
 
 #import "KDotAlert.h"
 
+#define SAFE_BLOCK(KDotBlock) if(KDotBlock) KDotBlock
+
+@interface KDotAlert ()
+@property (nonatomic, strong) UIAlertController *alertController;
+@property (nonatomic, assign) BOOL isAddCancelAction;
+@end
+
 @implementation KDotAlert
+
++ (instancetype)alert {
+    return [[KDotAlert alloc] initWithStyle:UIAlertControllerStyleAlert];
+}
+
++ (instancetype)actionSheet {
+    return [[KDotAlert alloc] initWithStyle:UIAlertControllerStyleActionSheet];
+}
+
+- (instancetype)initWithStyle:(UIAlertControllerStyle)style;
+{
+    if (self) {
+        _alertController = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:style];
+    }
+    return self;
+}
+
+- (KDotAlertStringBlock)title {
+    return ^(NSString *value) {
+        _alertController.title = value;
+        return self;
+    };
+}
+
+- (KDotAlertStringBlock)message {
+    return ^(NSString *value) {
+        _alertController.message = value;
+        return self;
+    };
+}
+
+- (KDotAlertActionBlock)defaultAction {
+    return ^(NSString * title, void (^ _Nullable hanlder)(UIAlertAction * _Nonnull action)) {
+        UIAlertAction *action = [UIAlertAction actionWithTitle:title style:UIAlertActionStyleDefault handler:hanlder];
+        [_alertController addAction:action];
+        return self;
+    };
+}
+
+- (KDotAlertActionBlock)cancelAction {
+    return ^(NSString * title, void (^ _Nullable hanlder)(UIAlertAction * _Nonnull action)) {
+        NSAssert(_isAddCancelAction == NO, @"You can only add cancelAction once!");
+        _isAddCancelAction = YES;
+        
+        UIAlertAction *action = [UIAlertAction actionWithTitle:title style:UIAlertActionStyleCancel handler:hanlder];
+        [_alertController addAction:action];
+        return self;
+    };
+}
+
+- (KDotAlertActionBlock)destructiveAction {
+    return ^(NSString * title, void (^ _Nullable hanlder)(UIAlertAction * _Nonnull action)) {
+        UIAlertAction *action = [UIAlertAction actionWithTitle:title style:UIAlertActionStyleDestructive handler:hanlder];
+        [_alertController addAction:action];
+        return self;
+    };
+}
+
+- (KDotAlertSubviewsBlock)actions {
+    return ^(void (^ _Nullable hanlder)(NSArray * _Nullable subViews)) {
+        SAFE_BLOCK(hanlder)(_alertController.actions);
+        return self;
+    };
+}
+
+- (KDotAlertMakePreferredActionBlock)makePreferredAction {
+    return ^(void) {
+        if (@available(iOS 9.0, *)) {
+            _alertController.preferredAction = _alertController.actions.lastObject;
+        } else {
+            // Fallback on earlier versions
+            NSAssert(NO, @"makePreferredAction only support iOS9 or later");
+        }
+        return self;
+    };
+}
+
+- (KDotAlertTextFieldBlock)addTextField {
+    return ^(void (^ _Nullable hanlder)(UITextField * _Nonnull textField)) {
+        NSAssert(_alertController.preferredStyle == UIAlertControllerStyleAlert, @"addTextField only support UIAlertControllerStyleAlert");
+        [_alertController addTextFieldWithConfigurationHandler:hanlder];
+        return self;
+    };
+}
+
+- (KDotAlertSubviewsBlock)textFields {
+    return ^(void (^ _Nullable hanlder)(NSArray * _Nullable subViews)) {
+        SAFE_BLOCK(hanlder)(_alertController.textFields);
+        return self;
+    };
+}
+
+- (KDotAlertShowBlock)show {
+    return ^(UIViewController *viewController, id block) {
+        NSAssert(viewController != nil, @"viewController connot be nil");
+        [viewController presentViewController:_alertController animated:YES completion:block];
+        return self;
+    };
+}
+
+
 
 @end
